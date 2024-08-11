1d6a8940dd3e6df33cd0636464aa914f0bceb25c
priyagupta-js <priyagupta052k@gmail.com>
2024-08-11 22:31:43 +0530

    Js file added

--- index.js
+++ index.js
@@ -1,2 +1,30 @@
+// grid = document.getElementById("grid");
 
+increment = document.getElementById("increment");
+decrement = document.getElementById("decrement");
+fontSize = document.getElementById("fontSize");
+
+let fontNumber = fontSize.innerHTML;
+
+
+// console.log(fontNumber);
+// function createTable(){
+//     let table = document.createElement('table');
+//     let tbody = document.createElement('tbody');
+
+//     for (let i=0;i<=26;i++)
+//     {
+//         let rows = document.createElement('tr');
+//         for(j=0;j<=100;i++)
+//         {
+//             let columns = document.createElement('td');
+//         rows.appendChild(columns);
+//         }
+//     }
+//     tbody.appendChild(rows);
+//     table.appendChild(tbody);
+//     grid.appendChild(table);
+// }
+
+// createTable();
 
